e4c9a35772e37e0f47b1661917955e2c8bf6a4a0
Solomon Ochepa <solomonochepa@gmail.com>
2025-06-24 17:15:32 +0100

    Refactor settings migration to use anonymous class and change ID to UUID

--- src/migrations/2014_10_00_000000_create_settings_table.php
+++ src/migrations/2014_10_00_000000_create_settings_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateSettingsTable extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
@@ -12,13 +12,15 @@ class CreateSettingsTable extends Migration
     public function up(): void
     {
         Schema::create('settings', function (Blueprint $table) {
-            $table->id();
+            $table->uuid('id')->primary();
             $table->string('name');
             $table->json('value');
             $table->string('group')->default('default');
             $table->nullableUuidMorphs('settable');
             $table->timestamps();
             $table->softDeletes();
+
+            $table->unique(['name', 'group', 'settable_type', 'settable_id'], 'unique_settings');
         });
     }
 
@@ -29,4 +31,4 @@ class CreateSettingsTable extends Migration
     {
         Schema::dropIfExists('settings');
     }
-}
+};
