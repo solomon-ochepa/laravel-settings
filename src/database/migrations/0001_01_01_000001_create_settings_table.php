0be579f0597c2ce2edab6d808fba63ecb0bd7928
Solomon Ochepa <solomonochepa@gmail.com>
2025-07-22 06:26:00 +0100

    Set char limit and index the name and group field

--- src/database/migrations/0001_01_01_000001_create_settings_table.php
+++ src/database/migrations/0001_01_01_000001_create_settings_table.php
@@ -13,9 +13,9 @@ return new class extends Migration
     {
         Schema::create('settings', function (Blueprint $table) {
             $table->uuid('id')->primary();
-            $table->string('name');
+            $table->string('name', 32)->index();
             $table->json('value')->nullable();
-            $table->string('group')->default('default');
+            $table->string('group', 16)->index()->default('default');
             $table->nullableUuidMorphs('settable');
             $table->timestamps();
             $table->softDeletes();
